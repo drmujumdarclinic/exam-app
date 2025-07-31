66f455f57cab2ba483affc51a26936c6754784da
drmujumdarclinic.github.io <sumedhmujumdar@gmail.com>
2025-07-31 09:31:21 +0530

    Update result.js

--- result.js
+++ result.js
@@ -72,3 +72,17 @@ document.addEventListener("DOMContentLoaded", () => {
     doc.save(filename);
   });
 });
+
+function downloadPDF() {
+  const element = document.getElementById('resultSection');
+
+  const opt = {
+    margin:       0.5,
+    filename:     'Exam_Result.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
